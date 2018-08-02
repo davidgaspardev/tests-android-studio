b96d94c21bff81e56efeeeed0c69fcb257e9cd0d
David Gaspar <davidgaspar.dev@gmail.com>
2018-08-02 00:09:42 -0300

    Added callback (OnClickListener)

--- app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
+++ app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.davidgaspardev.testing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnWeb = (Button) findViewById(R.id.intentForWeb);
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Testing CALLBACK", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
