a9c9f6c48e123ef0b8a13f3aafe26e04712802f5
David Gaspar <davidgaspar.dev@gmail.com>
2018-08-02 00:36:41 -0300

    Added Itent Implict for Web

--- app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
+++ app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.davidgaspardev.testing;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if(!edtWeb.getText().toString().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Address is not empty", Toast.LENGTH_SHORT).show();
+
+                    Uri addr = Uri.parse(edtWeb.getText().toString());
+                    startActivity(new Intent(Intent.ACTION_VIEW, addr));
+
+                    Toast.makeText(MainActivity.this, "Intent Started", Toast.LENGTH_SHORT).show();
+
+                    edtWeb.setText(null);
 
                 }else {
                     Toast.makeText(MainActivity.this, "Address is empty", Toast.LENGTH_SHORT).show();
