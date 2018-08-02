58d25c80d5f93ee76638546329f665acf1c9b775
David Gaspar <davidgaspar.dev@gmail.com>
2018-08-02 00:24:25 -0300

    Security added for callback (OnClickListener)

--- app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
+++ app/src/main/java/com/example/davidgaspardev/testing/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText edtWeb = (EditText) findViewById(R.id.intentForWebUri);
+
         Button btnWeb = (Button) findViewById(R.id.intentForWeb);
         btnWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Testing CALLBACK", Toast.LENGTH_SHORT).show();
+
+                if(!edtWeb.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Address is not empty", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Address is empty", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
